e893a3af273f18975754ae650aba87ef9824b347
y.k <kishinoyousuke@mail.goo.ne.jp>
2018-09-02 09:32:04 +0000

    100

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,6 +4,6 @@ class ToppagesController < ApplicationController
       @user = current_user
       @micropost = current_user.microposts.build  # form_for 用
       @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
-    end
+     end
   end
 end
